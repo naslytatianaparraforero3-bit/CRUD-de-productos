8433b6dcf38e7c36b943c3b885571f0762918b28
devstiven12 <devstiven12@gmail.com>
2026-06-08 19:12:07 -0500

    feat: se termina los archivos de card

--- src/app/features/products/components/product-card/product-card.component.ts
+++ src/app/features/products/components/product-card/product-card.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Product } from '../../models/product.model';
 
 @Component({
   selector: 'app-product-card',
   standalone: false,
   templateUrl: './product-card.component.html',
-  styleUrl: './product-card.component.scss'
+  styleUrl: './product-card.component.scss',
 })
 export class ProductCardComponent {
+  @Input() product!: Product;
+  @Output() delete = new EventEmitter<number>();
+  @Output() select = new EventEmitter<Product>();
 
+  onDelete(event: Event): void {
+    event.stopPropagation();
+    this.delete.emit(this.product.id);
+  }
+
+  onSelect(): void {
+    this.select.emit(this.product);
+  }
 }
