d32db8f88f61558c0f65d3c207ac4bba997beb90
devstiven12 <devstiven12@gmail.com>
2026-06-08 20:13:33 -0500

    se termina los archivos de list y se solucionan problemas de nombres en archivos

--- src/app/features/products/components/product-list/product-list.component.ts
+++ src/app/features/products/components/product-list/product-list.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../models/product.model';
 
 @Component({
   selector: 'app-product-list',
   standalone: false,
   templateUrl: './product-list.component.html',
-  styleUrl: './product-list.component.scss'
+  styleUrl: './product-list.component.scss',
 })
 export class ProductListComponent {
+  constructor(private productService: ProductService) {}
 
+  get products(): Product[] {
+    return this.productService.getProducts();
+  }
+
+  eliminar(id: number): void {
+    this.productService.deleteProduct(id);
+  }
+
+  seleccionar(product: Product): void {
+    this.productService.selectProduct(product);
+  }
 }
--- src/app/features/products/products.module.ts
+++ src/app/features/products/products.module.ts
@@ -2,23 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ProductPageComponent } from './pages/product-page/product-page.component';
-import { ProductFromComponent } from './components/product-form/product-from.component';
+import { ProductFormComponent } from './components/product-form/product-form.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { ProductCardComponent } from './components/product-card/product-card.component';
 
 @NgModule({
   declarations: [
     ProductPageComponent,
-    ProductFromComponent,
+    ProductFormComponent,
     ProductListComponent,
-    ProductCardComponent
+    ProductCardComponent,
   ],
-  imports: [
-    CommonModule,
-    FormsModule
-  ],
-  exports: [
-    ProductPageComponent
-  ]
+  imports: [CommonModule, FormsModule],
+  exports: [ProductPageComponent],
 })
-export class ProductsModule { }
+export class ProductsModule {}
